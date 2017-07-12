cb57bc444c8eccb746f3b6fb40e5623808fc87b5
Walter Monecke <wmonecke@gmail.com>
2017-07-12 20:27:40 +0200

    Added conditional rendering of content to listitem that is triggered on touch

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Text } from 'react-native';
+import { Text, View, TouchableWithoutFeedback } from 'react-native';
 
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
 
+  renderDescription () {
+    const { library, expanded } = this.props;
+
+    if (expanded) {
+      return (
+        <CardSection>
+          <Text style={styles.textStyle}>
+            { library.description }
+          </Text>
+        </CardSection>
+      );
+    }
+  }
 
   render() {
     const { titleStyle } = styles;
-    console.log(this.props);
-    
+    const { id, title } = this.props.library;
+
     return (
-      <CardSection>
-        <Text style={titleStyle}> { this.props.library.title } </Text>
-      </CardSection>
+      <TouchableWithoutFeedback
+        onPress={() => this.props.selectLibrary(id)}
+      >
+        <View>
+          <CardSection>
+            <Text style={titleStyle}> { title } </Text>
+          </CardSection>
+          { this.renderDescription() }
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
@@ -24,7 +44,20 @@ const styles = {
   titleStyle: {
     fontSize: 18,
     paddingLeft: 15
+  },
+
+  textStyle: {
+    paddingLeft: 18,
+    paddingRight: 5
   }
 }
 
-export default connect(null, actions)(ListItem);
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.id;
+
+  return {
+    expanded: expanded
+  };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
